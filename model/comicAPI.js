02d375f687a989e6f8b52dde373180674341d9c0
alicespyglass <alicespyglass@gmail.com>
2017-06-20 11:01:18 +0100

    outline of api call in comic API model. create comic file in model

---
+++ model/comicAPI.js
@@ -0,0 +1,13 @@
+(function(exports) {
+
+  function ComicAPI () {
+
+    ComicAPI.prototype.getComic = function () {
+      // make API call
+    };
+
+  }
+
+exports.ComicAPI = ComicAPI;
+
+})(this);
