738a2941d236c09f293620b0e95979d080d7e2f3
alicespyglass <alicespyglass@gmail.com>
2017-06-20 11:26:55 +0100

    outline of comic list model

---
+++ model/comicList.js
@@ -0,0 +1,16 @@
+(function (exports) {
+  function ComicList() {
+    this.listofComics = [];
+    this.comicAPI = new ComicAPI;
+
+    ComicList.prototype.storeList = function () {
+      const listOfComics =
+      for (var i = 0; i < 10; i++) {
+
+      }
+    };
+
+  }
+
+  exports.ComicList = ComicList;
+})(this);
