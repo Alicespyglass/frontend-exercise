b6e0903a06d23f582142688aad669b7e75968d65
alicespyglass <alicespyglass@gmail.com>
2017-06-23 02:39:17 +0100

    comic api - create array of 10 comic title and thumbnail addresses

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 (function(exports) {
   function ComicAPI () {
 
-    const MARVEL = 'https://gateway.marvel.com/v1/public/comics?apikey=3cb62d086d5debdeea139095cbb07fe4&ts=redant&hash=140e85a50884cef76d614f6dacada288'
+    const MARVEL = 'https://gateway.marvel.com/v1/public/comics?apikey=3cb62d086d5debdeea139095cbb07fe4&ts=redant&hash=140e85a50884cef76d614f6dacada288';
+    const COMICRAW = '';
+    const COMICLIST = [];
 
     ComicAPI.prototype.httpGet = function () {
       let xmlHttp = new XMLHttpRequest();
@@ -13,7 +15,18 @@
     ComicAPI.prototype.comicData = function () {
       let data = this.httpGet();
       const dataJson = JSON.parse(data);
-      return dataJson;
+      this.COMICRAW = dataJson;
+    };
+
+    ComicAPI.prototype.filterData = function () {
+      let data = this.COMICRAW;
+      debugger;
+      for (let i = 0; i < 10; i++) {
+        COMICLIST.push([data.data.results[i].title, data.data.results[i].thumbnail.path]);
+        debugger;
+      }
+
+
     };
   }
 
---
+++ scripts/renderComic.js
@@ -0,0 +1,12 @@
+// (function(exports) {
+//   function
+// })
+//
+//   Template for list items:
+//   <li class="comic-item">
+//       <div class="comic-card">
+//           <img src="..." />
+//           <h2>Comic name</h2>
+//           <button class="button js-add">Add to favourites</button>
+//       </div>
+//   </li>
